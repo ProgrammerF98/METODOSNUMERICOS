86648291a14eeff8e3c86b28c0147d71f328bfb9
ProgrammerF98 <pacheco.solis@outlook.com>
2018-08-21 23:15:10 -0500

    Update SumaNumerosPequeños.py

--- SumaNumerosPequeños.py
+++ SumaNumerosPequeños.py
@@ -1,3 +1,3 @@
 def sum_two_smallest_nums(lst):
-	lst = sorted(numero for numero in lst if numero >= 0) #recprdar que Sorted es para poner del número más chico al más grande de una lista
+	lst = sorted(numero for numero in lst if numero >= 0) #recordar que Sorted es para ordenar del número más chico al más grande de una lista
 	return lst[0] + lst[1]
