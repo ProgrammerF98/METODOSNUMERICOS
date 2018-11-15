235d4f307bef26fed528481be348a303eb2fc6f6
ProgrammerF98 <pacheco.solis@outlook.com>
2018-11-14 19:49:52 -0600

    Create KATA_15_A.py

---
+++ KATA_15_A.py
@@ -0,0 +1,32 @@
+# Metodo de Euler
+# y' - e^x = 0 y(0) = 1 y(5) = ?
+# dy/dx = e^x
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x,y):
+    return 2*x*y #aquí poner derivada
+
+def euler(y0, x, h, f):
+    y = []
+    y.append(y0)
+    for i in range(1, len(x)):
+        y.append(y[i-1] + h*f(x[i-1], y[i-1])) #formula de euler
+    return y
+
+n = 6 #despejar n
+a = 1 #valor inicial de x
+b = 1.15 #valor de x que 
+h = 0.1
+x = np.linspace(a,b,n)
+y = euler(1,x,h,f)
+
+plt.plot(x,y,'g')
+#plt.plot(x,[math.exp(xi) for xi in x], 'b')
+plt.grid()
+
+print('Valor de h:', h)
+
+print('Valor x: ', x[-1])
+print('Valor calculado y: ', y[-1])
